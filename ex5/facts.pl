f1dc38925e99f89032b25cd6cc3071d3aef47c73
Jacob Beal <jacob.beal@ieee.org>
2019-05-02 12:35:43 +1200

    Added week 7

---
+++ ex5/facts.pl
@@ -0,0 +1,21 @@
+% X is the father-of Y
+father(philip, charles).
+father(philip, anne).
+father(philip, andrew).
+
+father(charles, william).
+father(charles, henry).
+
+father(mark, peter).
+father(mark, zara).
+
+% X is the mother-of Y
+mother(elisabeth, charles).
+mother(elisabeth, anne).
+mother(elisabeth, andrew).
+
+mother(diana, william).
+mother(diana, henry).
+
+mother(anne, peter).
+mother(anne, zara).
